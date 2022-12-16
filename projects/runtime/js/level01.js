fb935cada87b6192457a88a2a6135866dbd378d1
SarahWaits1 <Waits506@stcharles.k12.la.us>
2022-12-16 17:17:53 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -5,7 +5,7 @@ var level01 = function (window) {
     var draw = window.opspark.draw;
     var createjs = window.createjs;
 
-    window.opspark.runLevelInGame = function(game) {
+    window.opspark.runLevelInGame = function (game) {
         // some useful constants 
         var groundY = game.groundY;
 
@@ -13,7 +13,7 @@ var level01 = function (window) {
         // behavior of our game
         var levelData = {
             "name": "Robot Romp",
-            "number": 1, 
+            "number": 1,
             "speed": -3,
             "gameItems": [
                 { "type": "sawblade", "x": 400, "y": groundY },
@@ -27,7 +27,7 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
-            function createSawBlade (x, y){
+        function createSawBlade(x, y) {
             var hitZoneSize = 25;
             var damageFromObstacle = 10;
             var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
@@ -38,29 +38,29 @@ var level01 = function (window) {
             sawBladeHitZone.addChild(obstacleImage);
             obstacleImage.x = -25;
             obstacleImage.y = -25;
-            }
+        }
 
-            createSawBlade(800, 250);
-            createSawBlade(1190, 350);
-            createSawBlade(1600, 255);
+        createSawBlade(800, 250);
+        createSawBlade(1190, 350);
+        createSawBlade(1600, 255);
 
-            // function createFireball(x,y){ 
-            // var hitZoneSize = 25;
-            // var damageFromObstacle = 10;
-            // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-            // sawBladeHitZone.x = x;
-            // sawBladeHitZone.y = y;
-            // game.addGameItem(sawBladeHitZone);
-            // var fireball = draw.bitmap("img/images.png")
-            // sawBladeHitZone.addChild(fireball);
-            // fireball.x = -25;
-            // fireball.y = -25;
-            // }
-            
-            // createFireball(300, 200)
-            
+        // function createFireball(x,y){ 
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+        // sawBladeHitZone.x = x;
+        // sawBladeHitZone.y = y;
+        // game.addGameItem(sawBladeHitZone);
+        // var fireball = draw.bitmap("img/images.png")
+        // sawBladeHitZone.addChild(fireball);
+        // fireball.x = -25;
+        // fireball.y = -25;
+        // }
 
-            function createEnemy(x,y){
+        // createFireball(300, 200)
+
+
+        function createEnemy(x, y) {
             var enemy = game.createGameItem("enemy", 25);
             var redSquare = draw.rect(50, 50, "red");
             redSquare.x = -25;
@@ -81,37 +81,37 @@ var level01 = function (window) {
                 enemy.shrink();
             };
         }
-            createEnemy(400, groundY - 20);
-            createEnemy(800, groundY - 110);
-            createEnemy(1200, groundY - 115);
-            
-            function createReward(x,y){
-                var reward = game.createGameItem("reward", 25);
-                var goldSquare = draw.rect(50, 50, "gold");
-                goldSquare.x = -25;
-                goldSquare.y = -25;
-                reward.addChild(goldSquare);
-                reward.x = x;
-                reward.y = y;
-                game.addGameItem(reward);
-                reward.velocityX = -1;
-                // reward.rotationalVelocity = 2;
-    
-                reward.onPlayerCollision = function () {
-                    game.changeIntegrity(20);
-            };git
+        createEnemy(400, groundY - 20);
+        createEnemy(800, groundY - 110);
+        createEnemy(1200, groundY - 115);
+
+        function createReward(x, y) {
+            var reward = game.createGameItem("reward", 25);
+            var goldSquare = draw.rect(50, 50, "gold");
+            goldSquare.x = -25;
+            goldSquare.y = -25;
+            reward.addChild(goldSquare);
+            reward.x = x;
+            reward.y = y;
+            game.addGameItem(reward);
+            reward.velocityX = -1;
+            // reward.rotationalVelocity = 2;
+
+            reward.onPlayerCollision = function () {
+                game.changeIntegrity(20);
+            }; git
         }
 
-            createReward(500, groundY - 20);
-            createReward(910, groundY - 30);
-            createReward(1300, groundY - 40);
+        createReward(500, groundY - 20);
+        createReward(910, groundY - 30);
+        createReward(1300, groundY - 40);
 
         // DO NOT EDIT CODE BELOW HERE
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = level01;
